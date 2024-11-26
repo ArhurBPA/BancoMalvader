82b1fe66192c0155f9c798e9117fafa18efda3c1
1thallo <ithalloflu@gmail.com>
2024-11-26 04:28:21 -0300

    .

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -9,16 +9,11 @@ public class ConnectionFactory {
     // Dados para conexão com o banco
     private static final String url = "jdbc:mysql://localhost:3306/banco_malvader";
     private static final String usuario = "root";
-    private static final String senha = "fFLUZAO2004.";
+    private static final String senha = "";
 
     // Metodo para conectar com o banco
     public static Connection getConnection() throws SQLException {
-        try {
-            // Registrar o driver manualmentev
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Erro ao carregar o driver do MySQL!", e);
-        }
-        return DriverManager.getConnection(url, usuario, senha);
+        return DriverManager.getConnection(url, usuario, senha); // garante nova conexao
     }
+
 }
